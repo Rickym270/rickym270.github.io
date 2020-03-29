c5c211fc86d130add2322be699b4a15a7d4232c8
Ricky Martinez <martinez.ricky95@gmail.com>
2020-03-29 07:54:23 -0400

    Created default page load and started home.html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
     console.log("Nothing has been done yet");
+    var location_name = location.href.split("/")[3];
+    if(location_name == "index.html"){
+        jQuery("#content").load("home.html");
+    }
     jQuery(function(){
         jQuery("#menu_links > .col-4 > a").click(function(){            
             var sectionUrl = $(this).attr("data-url");
             jQuery("#content").load(sectionUrl, function(){
-                console.log("Loaded");
+                console.log("Loaded " + sectionUrl);
             })
         });
     });
