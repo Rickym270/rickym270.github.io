bb2e717ff65f097cfb43c2fa9716295893a2cb9d
Ricky Martinez <martinez.ricky95@gmail.com>
2020-03-29 03:21:46 -0400

    Edited structure, added homepage, encapsulted files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("Nothing has been done yet")
+$(document).ready(function(){
+    console.log("Nothing has been done yet");
+    $("#menu_links > .col-4 > a").one("click", function(){
+        var sectionUrl = $(this).attr("data-url");
+        var callMethod = "POST";
+        var dataHTML = "<h1>New Content</h1>";
+        
+        $.ajax({
+            url: sectionUrl,
+            type: callMethod,
+            cache: false,
+            data: {
+                'html': dataHTML
+            },
+            dataType: 'html',
+            success: function(htmlReturned) {
+                $("#content").html(htmlReturned);
+                console.log("Executed Once.");
+            }
+        });
+    });
+});
