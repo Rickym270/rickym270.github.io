ce708f8487b0299645981c8c5fce2e3a8c1fac1c
Ricky Martinez <martinez.ricky95@gmail.com>
2020-03-29 08:10:39 -0400

    Fixed jQuery hack and styling/positioning issues

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    console.log("Nothing has been done yet");
     var location_name = location.href.split("/")[3];
-    if(location_name == "index.html"){
+    if(location_name == "index.html" || location_name == ""){
         jQuery("#content").load("home.html");
     }
+    
     jQuery(function(){
         jQuery("#menu_links > .col-4 > a").click(function(){            
             var sectionUrl = $(this).attr("data-url");
