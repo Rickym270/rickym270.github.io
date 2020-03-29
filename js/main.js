8f863feccb6319b8c0fdf99dce5f4fb770204325
Ricky Martinez <martinez.ricky95@gmail.com>
2020-03-29 04:00:21 -0400

    ajax call not working. Use load instead

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
 $(document).ready(function(){
     console.log("Nothing has been done yet");
-    $("#menu_links > .col-4 > a").one("click", function(){
-        var sectionUrl = $(this).attr("data-url");
-        var callMethod = "POST";
-        var dataHTML = "<h1>New Content</h1>";
-        
-        $.ajax({
-            url: sectionUrl,
-            type: callMethod,
-            cache: false,
-            timeout : 100000,
-            dataType: 'html',
-            data: {
-                'html': dataHTML
-            },
-            success: function(htmlReturned) {
-                $("#content").html(htmlReturned);
-                console.log("Executed Once.");
-            }
+    jQuery(function(){
+        jQuery("#menu_links > .col-4 > a").click(function(){            
+            var sectionUrl = $(this).attr("data-url");
+            jQuery("#content").load(sectionUrl, function(){
+                console.log("Loaded");
+            })
         });
     });
 });
