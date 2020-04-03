843262b5405b018167b88397f6d11d9b1dee1562
Ricky Martinez <martinez.ricky95@gmail.com>
2020-04-03 10:51:44 -0400

    Added pages, edited navbar, structurem styles, js amd renamed files.

--- js/SPAHack.js
+++ js/SPAHack.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
     var location_name = location.href.split("/")[3];
+    
+    // Defaut load
     if(location_name == "index.html" || location_name == ""){
-        jQuery("#content").load("home.html");
+        jQuery("#content").load("pages/home.html");
     }
     
+    // Navbar Links
     jQuery(function(){
-        jQuery("#menu_links > .col-4 > a").click(function(){            
+        jQuery("a.nav-link").click(function(){            
             var sectionUrl = $(this).attr("data-url");
             jQuery("#content").load(sectionUrl, function(){
                 console.log("Loaded " + sectionUrl);
