705b8441a513f623ecd711aa87a15f7f0bb31c4f
root <root@RickysDesktop.localdomain>
2020-11-11 18:38:54 -0500

    Abstracted code for cleaner code

--- bin/GenNotes.py
+++ bin/GenNotes.py
@@ -1,15 +1,26 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 import sys
-o_Notes = '../data/RawFAQPages/'
+import os.path
+from os import path
+
+
+sys.path.append("exceptions")
+from Exceptions import InvalidCommandFormat
+from Exceptions import UnknownPath
 
 args = sys.argv[1:]
 print("Arguments: {}".format(args))
 
-class InvalidCommandFormat(Exception):
-    pass
+if not args or len(args)<2:
+    raise InvalidCommandFormat('src_file=<path> and out_file=<path> options needed')
 
-#if '--src_file' in args:
+if ('--src_file' in args and '--out_file' not in args) or ('--out_file' in args and '--src_file' not in args):
+    raise InvalidCommandFormat('src_file and out_file needed together')
 
-if '--out_file' in args and '--src_file' not in args:
-    raise InvalidCommandFormat('TESTING')
-
+#TODO: Does the path exist?
+src = args[0].split("=")[1]
+dest = args[1].split("=")[1]
+if not path.exists(src):
+    raise UnknownPath("Src Path {} is not a real/valid location".format(src))
+if not path.exists(dest):
+    raise UnknownPath("Out Path {} is not a real/valid location".format(dest))
---
+++ bin/exceptions/Exceptions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#NOTE: Custom exceptions
+class InvalidCommandFormat(Exception):
+    pass
+
+class UnknownPath(Exception):
+    pass
