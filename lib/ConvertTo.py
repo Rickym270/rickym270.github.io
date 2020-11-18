696c30701468c40a6973efb29518ebede488f03d
Ricky Martinez <martinez.ricky95@gmail.com>
2020-11-18 13:18:37 -0500

    Working on parsing to HTML

--- lib/ConvertTo.py
+++ lib/ConvertTo.py
@@ -8,32 +8,59 @@ sys.path.append("../bin/exceptions/")
 from Exceptions import InvalidFileObject
 
 class ConvertTo(object):
-    def __init__(self, fobj):
-        self.TEMPLATE = "../templates/doc.html"
+    def __init__(self, doc_type, fobj):
         self.TEMPLATE_DIR = "../templates/"
+        #NOTE: Check if doc_type is passed
+        supported_types = ["Docs", "Projects"]
+        if doc_type in supported_types: pass
+        else: raise InvalidDocType("Unable to generate {}".format(doc_type))
+
         # TODO: Check if a file object passed
         import io
-        if type(fobj) == io.TextIOWrapper:
-            pass
+        if type(fobj) == io.TextIOWrapper: pass
+        else: raise InvalidFileObject("Expected io.TextIOWrapper but received {}".format(type(fobj)))
+
+        if doc_type == "Docs":
+            self.TEMPLATE = "doc.html"
         else:
-            raise InvalidFileObject("Expected io.TextIOWrapper but received {}".format(type(fobj)))
-        
+            self.TEMPLATE = "projects.html"
+        self.doc_type = doc_type
         self.lines = fobj.readlines()
 
     def html(self):
         '''
             Convert self.lines to HTML
         '''
-        print("cvt.html running. Self.lines: {}".format(self.lines))
-        pycode_start = False
-        title = self.lines[0]
-        for i in range(len(self.lines)):
-            # NOTE: Define if python code has started
-            if '<pycode>' in self.lines[i]:
-                pycode_start = True
-            if '</pycode>' in self.lines[i] and pycode_start:
-                pycode_start = False
+        print("cvt.html running to generate {}".format(self.doc_type))
+        import jinja2
 
-        print("Title: {}".format(title))
+        template = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=self.TEMPLATE_DIR))
+        template = template.get_template(self.TEMPLATE)
+
+        pycode_start = False
+        devider = False
+        #NOTE: Title has to be first in the line
+        title = self.lines[0].strip()
+        #NOTE: Body is the rest minus the first line
+        body = self.lines[1:]
+        for i in range(len(body)):
+            if body[i] == "\n": body[i] = "</br>"
+            #NOTE: Interpret '=====' as a divider
+            if "=====" in body[i]:
+                body[i] = "<div class='divider'></div>"
+
+            if pycode_start:
+                body[i] = "<code>{}</code>".format(body[i])
+            # NOTE: Define if python code has started
+            if '<pycode>' in body[i]:
+                pycode_start = True
+                #NOTE: Replace <pycode> with <pre>
+                body[i] = "<pre>"
+            if '</pycode>' in body[i] and pycode_start:
+                pycode_start = False
+                #NOTE: Replace <pycode> with <pre>
+                body[i] = "</pre>"
+        
+        return template.render(title=title, content=body)
 
 
