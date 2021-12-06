05d0816a829b796b9c99ba870a48e70986deda11
Rickym270 <martinez.ricky95@gmail.com>
2021-12-05 23:28:03 -0500

    Added practice for sorted squared array problem

---
+++ data/projects/InterviewStudying/SortedSquaredArray/Practice3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def sortedSquaredArray(array):
+    squared_array = [0 for _ in array]
+    lp = 0
+    rp = len(array) - 1
+
+    for idx in reversed(range(len(array))):
+        if abs(array[lp]) > abs(array[rp]):
+            squared_array[idx] = array[lp] ** 2
+            lp += 1
+        else:
+            squared_array[idx] = array[rp] ** 2
+            rp -= 1
+    return squared_array
+
+if __name__ == "__main__":
+    print(sortedSquaredArray([-6,-3,0,1,2]))
+
