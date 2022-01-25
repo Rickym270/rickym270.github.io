dc8c7ac181f3e2b120e8fbfe59d1c1c06b8788dd
Ricky Martinez <rmartinez@RickysDesktop.localdomain>
2022-01-24 19:17:34 -0500

    Added practice

--- data/projects/InterviewStudying/MetaPreparation/PairSums/PairSums.py
+++ data/projects/InterviewStudying/MetaPreparation/PairSums/PairSums.py
@@ -15,12 +15,13 @@ targetSum2 = 6
 # 3rd 5th
 
 
-# 2+2 (1st, 2nd)
-# 2+2 (1st, 3rd)
-# 2+2 (1st, 4th)
+# 2+2 (1st, 2nd) seen: TWO new 2's, subtract one because we only want to count the 2 one time. Seperate but equal
+# 2+2 (1st, 3rd) seen: ONE mew 2, ONE old 2. We only care about the new one so subtract one
+# 2+2 (1st, 4th) seen: ONE mew 2, ONE old 2. We only care about the new one so subtract one
 
-# 2+2 (2nd, 3rd)
-# 2+2 (2nd, 4th)
+# 2+2 (2nd, 3rd) seen: ONE old 2(we saw this in the previous iteration (^). ONE new 3. We only care about the new one
+#               q       so subtract one.
+# 2+2 (2nd, 4th) seen: ONE old 2, ONE new 2. We only care about the new one so subtract one
 
 # 2+2 (3rd, 4th)
 array3 = [2, 2, 2, 2]
@@ -49,7 +50,12 @@ def numberOfWays(arr, k):
 
         # TODO: Add number of times seen to the count
         count += seen[needed_number]
+        print("count->{}    needed number {}, there are {} needed numbers in seen\n".format(count, needed_number,
+                                                                                            seen[needed_number]))
+        # TODO: If number needed, is the current number, subtract 1
+        #           We do this because we only want to count the pairs (2 numbers = 1 pair,
         if needed_number == arr[i]:
+            print("IF needed_number = {}, TRUE, subtracting one from count".format(needed_number))
             count -= 1
 
     return count // 2
