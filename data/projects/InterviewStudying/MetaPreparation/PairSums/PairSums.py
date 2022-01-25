502f6688b3fb50e8e40c838dfd758b87e3206302
Ricky Martinez <rmartinez@RickysDesktop.localdomain>
2022-01-24 19:54:31 -0500

    More studying

--- data/projects/InterviewStudying/MetaPreparation/PairSums/PairSums.py
+++ data/projects/InterviewStudying/MetaPreparation/PairSums/PairSums.py
@@ -5,26 +5,27 @@
     considered different if one pair includes at least one array index which the other doesn't, even if they include the
     same values.
 '''
+# Returns 2
 array1 = [1, 2, 3, 4, 3]
 targetSum1 = 6
 
+# Returns 4
 array2 = [1, 5, 3, 3, 3]
 targetSum2 = 6
 # 1st 2nd
 # 3rd 4th
 # 3rd 5th
 
-
-# 2+2 (1st, 2nd) seen: TWO new 2's, subtract one because we only want to count the 2 one time. Seperate but equal
-# 2+2 (1st, 3rd) seen: ONE mew 2, ONE old 2. We only care about the new one so subtract one
-# 2+2 (1st, 4th) seen: ONE mew 2, ONE old 2. We only care about the new one so subtract one
-
-# 2+2 (2nd, 3rd) seen: ONE old 2(we saw this in the previous iteration (^). ONE new 3. We only care about the new one
-#               q       so subtract one.
-# 2+2 (2nd, 4th) seen: ONE old 2, ONE new 2. We only care about the new one so subtract one
-
-# 2+2 (3rd, 4th)
 array3 = [2, 2, 2, 2]
+# 2+2 (1st, 2nd) seen: TWO new 2's, subtract one because we only want to count the 2 one time.
+# 2+2 (1st, 3rd) seen: ONE new 2, ONE old 2. We only care about the new one so subtract one
+# 2+2 (1st, 4th) seen: ONE new 2, ONE old 2. We only care about the new one so subtract one
+
+# 2+2 (2nd, 3rd) seen: ONE new 3, ONE old 2 (we saw this in the previous iteration (^). We only care about the new one
+#               q       so subtract one.
+# 2+2 (2nd, 4th) seen: ONE new 2, ONE old 2. We only care about the new one so subtract one
+
+# 2+2 (3rd, 4th) seen: ZERO new 2, TWO old 2's. We don't count it.
 targetSum3 = 4
 
 def numberOfWays(arr, k):
@@ -49,15 +50,23 @@ def numberOfWays(arr, k):
             continue
 
         # TODO: Add number of times seen to the count
+        #           We do this because by adding the number of times a number is seen, we account for every number seen
         count += seen[needed_number]
         print("count->{}    needed number {}, there are {} needed numbers in seen\n".format(count, needed_number,
                                                                                             seen[needed_number]))
         # TODO: If number needed, is the current number, subtract 1
-        #           We do this because we only want to count the pairs (2 numbers = 1 pair,
+        #           We do this because we only want to count the pairs (2 same numbers = 1 total number,
         if needed_number == arr[i]:
             print("IF needed_number = {}, TRUE, subtracting one from count".format(needed_number))
             count -= 1
 
+    # NOTE: We divide by 2.
+    #           Why? Since every number is accounted for, and we only want # of pairs, we do an integer division
+    #               (no remainder)
+    #               Why?
+    #                   Since we are subtracting 1 from every pair of the same number we have, since we only want the
+    #                   pair number count, there could be a decimal number when dividing by 2. You can't have .5 of a
+    #                   pair so do an integer division to drop the decimal number
     return count // 2
 
 if __name__ == "__main__":
