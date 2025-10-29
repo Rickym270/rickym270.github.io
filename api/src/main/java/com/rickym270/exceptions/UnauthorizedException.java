8b24956b443f1dfb09c9b9dc1b5ac9dfe09eb42f
Rickym270 <martinez.ricky95@gmail.com>
2025-10-29 18:06:57 -0400

    Add human-readable 404/422/400/401/405/415 handlers; enable NoHandlerFoundException

---
+++ api/src/main/java/com/rickym270/exceptions/UnauthorizedException.java
@@ -0,0 +1,10 @@
+package com.rickym270.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
+
+
+
